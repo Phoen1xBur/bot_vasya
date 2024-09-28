90c8488ed0768c21816b555d5c4f4c57dd6abfae
Artem <artem-121311@mail.ru>
2024-09-28 14:38:04 +0300

    Check is not None text

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import logging
 import random
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.enums import ChatType
+from aiogram.enums import ChatType, ContentType
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -23,15 +23,19 @@ async def start(message: Message):
 
 @dp.message(
     ChatTypeFilter(chat_type=[ChatType.GROUP, ChatType.SUPERGROUP]),
-    F.text & (F.text is not None) &
+    F.text &
+    (F.content_type == ContentType.TEXT) &
     F.text[0] != '/'
 )
 async def echo(message: Message):
+    if message.text is None:
+        return
     await Messages.insert_message(message.chat.id, message.text)
 
-    if random.randint(0, 100) < 3:
+    if random.randint(0, 100) <= 20:
         messages = [msg.text for msg in await Messages.get_messages(message.chat.id)]
         text = generate_text(messages)
+        print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {text}')
         await message.answer(text)
 
 
